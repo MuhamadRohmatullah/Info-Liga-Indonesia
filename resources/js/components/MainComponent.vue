48de46e5585503820c9dd62af5112fa01f7d52fd
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-04-13 20:06:03 +0700

    menambahkan card dan tabel unutk menampilkan peringkat tim

--- resources/js/components/MainComponent.vue
+++ resources/js/components/MainComponent.vue
@@ -1,83 +1,34 @@
 <template>
- 
-    <div class="container">
-    <div class="card">
-       <table class="table">
-            <thead>
-                <tr>
-                    <th>Position</th>
-                    <th>Club</th>
-                    <th>Match</th>
-                    <th>M</th>
-                    <th>D</th>
-                    <th>L</th>
-                    <th>Points</th>
-                    <th>Goals</th>
-                    <th>Form</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(data, index) in jadwal" :key="index">
-                    <td>{{data.position}}</td>
-                    <td>{{data.club}}</td>
-                    <td>{{data.matches}}</td>
-                    <td>{{ data.win }}</td>
-                    <td>{{ data.draw }}</td>
-                    <td>{{ data.lose }}</td>
-                    <td>{{ data.points }}</td>
-                    <td>{{ data.goals }}</td>
-                    <td>{{ data.form }}</td>
-                </tr>
-            </tbody>
-       </table>
-       </div>
+  <headerComponent />
+  <div class="container">
+    <div class="card my-2">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th class="judul">
+              <v-icon icon="mdi-medal-outline" style="color: white" />
+            </th>
+            <th class="judul">Club</th>
+            <th class="judul text-center">Tanding</th>
+            <th class="judul">M</th>
+            <th class="judul">D</th>
+            <th class="judul">K</th>
+            <th class="judul">Point</th>
+            <th class="judul">SG</th>
+            <th class="judul">Laga terakhir</th>
+          </tr>
+        </thead>
+        <tbody>
+         
+        </tbody>
+      </table>
+
     </div>
+  </div>
 </template>
 <script setup>
-import {computed, onMounted, ref} from 'vue';
-import {useStore} from 'vuex';
-const count = ref(0);
-const a = 'attention please !';
-
-const model = defineModel();
-const store = useStore();
-
-
-const not = computed(()=>{
-    return store.getters.getNote
-})
-
-const jadwal = computed(()=>{
-    return store.getters.getJad
-})
-
-function attention(){
-    for(let i=0; i<=5; i++){
-         console.log(a+i);
-    }
-}
-
-function increment(){
-    count.value++
-}
-
-function respon(){
-    console.log('emmit succesfull !');
-}
-
-const com = computed(() =>{ return parseInt(count.value)})
-
-function tes(){
-    console.log('testssss');
-}
-
-onMounted(()=>{
-    console.log('COmponent mounted !');
-    store.dispatch('getJadwal');
-})
-
-console.log(jadwal)
-
-
 
 </script>
+<style scoped>
+
+</style>
