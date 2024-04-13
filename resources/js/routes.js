b053aa1f7b5743be10d08bcc1f62d22b969f43a0
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-04-13 19:44:56 +0700

    menambahkan route klasemen, news, allnews, jadwal, teams dan klub dan menmapilkan masing - masing komponennya dengan lazy load

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,5 +1,4 @@
 import ExampleComponent from './components/ExampleComponent.vue';
-import MainComponent from './components/MainComponent.vue';
 import TestComponent from './components/HeaderComponent.vue';
 
 export const routes = [
@@ -7,11 +6,36 @@ export const routes = [
     {
         path : '/',
         name : 'home',
-        component : MainComponent
+        component : () => import('./components/ExampleComponent.vue')
     },
     {
-        path : '/test',
-        name : 'test',
-        component : TestComponent
+        path : '/klasemen',
+        name : 'klasemen',
+        component : () => import('./components/MainComponent.vue')
+    },
+    {
+        path : '/news',
+        name : 'news',
+        component : () => import('./components/NewsComponent.vue')
+    },
+    {
+        path: '/allNews',
+        name : 'allNews',
+        component :  () => import('./components/AllNewsComponent.vue')
+    },
+    {
+        path: '/jadwal',
+        name : 'jadwal',
+        component :  () => import('./components/ScheduleComponent.vue')
+    },
+    {
+        path: '/teams',
+        name : 'teams',
+        component :  () => import('./components/TeamComponenet.vue')
+    },
+    {
+        path : '/club',
+        name : 'club',
+        component : () => import('./components/ClubComponent.vue'),
     }
 ]
