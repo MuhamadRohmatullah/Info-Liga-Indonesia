b1a5027f25637b99d5de22857ac29d56a2acb7be
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-04-14 17:22:27 +0700

    Menambahkan tombol untuk kembali ke halaman tim, menambahkan card unutk menampilkan data pemain dari getters getPlayer

--- resources/js/components/ClubComponent.vue
+++ resources/js/components/ClubComponent.vue
@@ -1,6 +1,8 @@
 <template>
   <headerComponent />
- 
+  <div @click="goToBack" class="backBtn">
+    <v-icon icon="mdi-chevron-double-left" size="30px" /><span>Back</span>
+  </div>
   <div class="card mt-2 mx-1">
     <div class="card-body fs-4">
       <blockquote class="blockquote">
@@ -45,6 +47,48 @@
     </div>
   </div>
 
+  <div class="row mt-2">
+    <div
+      class="col-xl-3 mt-1 text-center"
+      v-for="(element, index) in player"
+      :key="index"
+    >
+      <div class="card">
+        <div class="card-body">
+          <h5>
+            {{ element.player_name }}
+          </h5>
+          <div class="fw-bold">
+            <v-icon icon="mdi-tshirt-crew-outline" />{{ element.player_number }}
+          </div>
+          <h5
+            v-if="element.position == 'Goalkeeper'"
+            style="color: red; font-weight: bold"
+          >
+            {{ element.position }}
+          </h5>
+          <h5
+            v-else-if="element.position == 'Midfielder'"
+            style="color: green; font-weight: bold"
+          >
+            {{ element.position }}
+          </h5>
+          <h5
+            v-else-if="element.position == 'Forward'"
+            style="color: red; font-weight: bold"
+          >
+            {{ element.position }}
+          </h5>
+          <h5
+            v-else-if="element.position == 'Defender'"
+            style="color: orange; font-weight: bold"
+          >
+            {{ element.position }}
+          </h5>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
 import { computed } from "vue";
@@ -58,7 +102,15 @@ const club = computed(() => {
   return store.getters.getClub;
 });
 
+const player = computed(() => {
+  return store.getters.getPlayer;
+});
 
+const router = useRouter();
+
+function goToBack() {
+  router.push({ name: "teams" });
+}
 </script>
 <style scoped>
 .card {
