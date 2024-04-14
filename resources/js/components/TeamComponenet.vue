3908520faad210077f0637bca0959cc8fd8c1821
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-04-14 17:11:30 +0700

    Menginport gambar, melakukan filter pengondisian dengan membuat fungsi logo dan menampilkannya di img card

--- resources/js/components/TeamComponenet.vue
+++ resources/js/components/TeamComponenet.vue
@@ -24,6 +24,86 @@
 import headerComponent from "./HeaderComponent.vue";
 import { computed, onBeforeMount } from "vue";
 import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+
+import psis from "../../../public/img/team_logo/818.png";
+var logopsis = psis;
+import persib from "../../../public/img/team_logo/471.png";
+var logopersib = persib;
+import persebaya from "../../../public/img/team_logo/684.png";
+var logopersebaya = persebaya;
+import psm from "../../../public/img/team_logo/724.png";
+var logopsm = psm;
+import bali from "../../../public/img/team_logo/472.png";
+var logobali = bali;
+import rans from "../../../public/img/team_logo/853.png";
+var logorans = rans;
+import persita from "../../../public/img/team_logo/817.png";
+var logopersita = persita;
+import pss from "../../../public/img/team_logo/493.png";
+var logopss = pss;
+import borneo from "../../../public/img/team_logo/473.png";
+var logoborneo = borneo;
+import bhayangkara from "../../../public/img/team_logo/777.png";
+var logobhayangkara = bhayangkara;
+import persija from "../../../public/img/team_logo/778.png";
+var logopersija = persija;
+import persisolo from "../../../public/img/team_logo/842.png";
+var logopersisolo = persisolo;
+import arema from "../../../public/img/team_logo/748.png";
+var logoarema = arema;
+import madura from "../../../public/img/team_logo/685.png";
+var logomadura = madura;
+import persik from "../../../public/img/team_logo/775.png";
+var logopersik = persik;
+import barito from "../../../public/img/team_logo/819.png";
+var logobarito = barito;
+import dewa from "../../../public/img/team_logo/846.png";
+var logodewa = dewa;
+import persikabo from "../../../public/img/team_logo/820.png";
+var logopersikabo = persikabo;
+
+function logo(id) {
+  if (id == "471") {
+    return logopersib;
+  } else if (id == "818") {
+    return logopsis;
+  } else if (id == "684") {
+    return logopersebaya;
+  } else if (id == "724") {
+    return logopsm;
+  } else if (id == "472") {
+    return logobali;
+  } else if (id == "853") {
+    return logorans;
+  } else if (id == "817") {
+    return logopersita;
+  } else if (id == "493") {
+    return logopss;
+  } else if (id == "473") {
+    return logoborneo;
+  } else if (id == "777") {
+    return logobhayangkara;
+  } else if (id == "778") {
+    return logopersija;
+  } else if (id == "842") {
+    return logopersisolo;
+  } else if (id == "748") {
+    return logoarema;
+  } else if (id == "685") {
+    return logomadura;
+  } else if (id == "775") {
+    return logopersik;
+  } else if (id == "819") {
+    return logobarito;
+  } else if (id == "846") {
+    return logodewa;
+  } else if (id == "820") {
+    return logopersikabo;
+  }
+}
+
+const store = useStore();
 
 onBeforeMount(() => {
   store.dispatch("getTeam");
