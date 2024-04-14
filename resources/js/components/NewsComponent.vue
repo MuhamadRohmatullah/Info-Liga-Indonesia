de4a5c0f25141bd64d53bef98fbfbfc7b395f0bd
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-04-14 16:22:18 +0700

    membuat halaman newscomponent, menambahkan component header dan menambahkan container dan row

---
+++ resources/js/components/NewsComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <headerComponent />
+  <div class="container-fluid">
+    <div class="row">
+     
+    </div>
+    <div class="row text-end mt-0">
+      <h5>
+       
+      </h5>
+    </div>
+   
+  </div>
+</template>
+<script setup>
+
+</script>
+<style>
+
+</style>
