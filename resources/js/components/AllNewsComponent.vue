1600af694c7bf1f3556461960507c40111198d99
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-04-14 16:33:52 +0700

    Menambahkan component headercomponent, menambahkan card unutk menampilkan semua data yang diambil dari getters getBerita, menambahkan gambar dan mendispatch action getNews

---
+++ resources/js/components/AllNewsComponent.vue
@@ -0,0 +1,59 @@
+<template>
+  <headerComponent />
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xl-6 mt-2" v-for="(element, index) in news" :key="index">
+        <a :href="element.url" class="link">
+          <div class="card">
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <img :src="imgrl" alt="gambar" style="width: 90%" />
+                </div>
+                <div class="col">
+                  <h5 class="fw-bold">{{ element.title }}</h5>
+                </div>
+              </div>
+            </div>
+            <div class="card-title text-center px-2"></div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import headerComponent from "./HeaderComponent.vue";
+import { onBeforeMount, computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const news = computed(() => {
+  return store.getters.getBerita;
+});
+
+import imgUrl from "../../../public/img/Liga_1.svg";
+
+const imgrl = imgUrl;
+onBeforeMount(() => {
+  store.dispatch("getNews");
+});
+</script>
+<style scoped>
+.gbr {
+  padding: 50px;
+  width: auto;
+  height: auto;
+  border: 2px solid black;
+}
+
+.card {
+  border-radius: 0;
+  height: 100%;
+}
+
+.link {
+  text-decoration: none;
+}
+</style>
